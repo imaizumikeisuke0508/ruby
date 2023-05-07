0944d62f0a09cf71b0ccdd5d78b2528f79fb73a9
EC2 Default User <ec2-user@ip-172-31-42-251.ap-northeast-1.compute.internal>
2023-05-07 07:51:13 +0000

    fix lesson5

--- lesson5.rb
+++ lesson5.rb
@@ -1,10 +1,12 @@
-total_price = "100"
+total_price = 90
 
-if total_price == "100"
-  puts "みかんを購入、所持金は0円"
-elsif total_price >= "100"
+if total_price > 100
   puts "みかんを購入、所持金が余りました"
-elsif total_price <= "100"
+
+elsif  total_price == 100
+  puts "みかんを購入、所持金は0円"
+
+elsif total_price < 100
   puts "みかんを購入することは出来ませんでした"
 
 end
