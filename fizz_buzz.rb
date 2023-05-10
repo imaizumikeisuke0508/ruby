a06d24f9931a5ffa3d959939d8b806db398fb846
EC2 Default User <ec2-user@ip-172-31-42-251.ap-northeast-1.compute.internal>
2023-05-10 09:31:02 +0000

    mk

---
+++ fizz_buzz.rb
@@ -0,0 +1,24 @@
+def fizz_buzz(number)
+ puts "数字を入力してください"
+ if (input / 3) =
+   if (input / 5) =
+     puts "fizzbuzz!"
+   end
+   puts "fizz"
+   elsif (input / 5) =
+     puts "buzz"
+   else
+     puts "#(input)"
+
+ end
+
+
+puts "結果は…"
+end
+input = gets
+# 数字を入力
+# 数字が３で割り切る→truw-fizz
+# 数字が５で割り切る→true-buzz
+# 両方で割り切れる→fizzbu
+# どれにも該当しない→数値を文字列に変えて返す
+
