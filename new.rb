e174fea9d390c3114b22cad6128283e3ea11df54
EC2 Default User <ec2-user@ip-172-31-42-251.ap-northeast-1.compute.internal>
2023-05-11 12:54:08 +0000

    change

--- new.rb
+++ new.rb
@@ -7,12 +7,13 @@ class Car
   def run(distance)
     puts "車で#{distance}キロ走ります"
   end
-  def turn(way)
+  def self.turn(way)
     puts "#{way}に曲がります"
-
   end
 
 end
 car = Car.new
 car.run(5)
 car.move("右",5)
+
+car.turn("右")
